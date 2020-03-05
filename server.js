725adc09c631d5877d0bfbc3260fc9acd94b29f0
Blaine Johnson <lindonbej@gmail.com>
2020-03-05 11:53:51 -0700

    added error checking in post request

--- server.js
+++ server.js
@@ -25,17 +25,19 @@ const Student = mongoose.model('Student', {
 // CORS support
 
 server.use(cors());
-// var corsOptions = {
-// 	// warning: accepts requests from all origins
-// 	origin: /.*/,
-// 	optionsSuccessStatus: 200
-// };
+var corsOptions = {
+	// warning: accepts requests from all origins
+	origin: /.*/,
+	optionsSuccessStatus: 200
+};
 
-server.get("/majors", (req, res) => {
+// GET
+
+server.get("/majors", cors(corsOptions), (req, res) => {
 	res.json({"majors": data.majors});
 });
 
-server.get("/majorProgram/:majorID", (req, res) => {
+server.get("/majorProgram/:majorID", cors(corsOptions), (req, res) => {
 	const majorID = req.params.majorID;
 
 	if (majorID == CS_ID) {
@@ -48,7 +50,7 @@ server.get("/majorProgram/:majorID", (req, res) => {
 	}
 });
 
-server.get("/majorSchedule/:majorID", (req, res) => {
+server.get("/majorSchedule/:majorID", cors(corsOptions), (req, res) => {
 	const majorID = req.params.majorID;
 
 	if (majorID == CS_ID) {
@@ -61,7 +63,7 @@ server.get("/majorSchedule/:majorID", (req, res) => {
 	}
 });
 
-server.get("/schedule/:netid", (req, res) => {
+server.get("/schedule/:netid", cors(corsOptions), (req, res) => {
 	console.log("entered get");
 	mongoose.connect('mongodb://localhost:27017/StudentDB', {
 		useNewUrlParser: true
@@ -81,93 +83,28 @@ server.get("/schedule/:netid", (req, res) => {
 
 // POST
 
-server.post("/schedule/:netid", (req, res) => {
-	schedule = JSON.stringify(req.body);
-	netid = req.params.netid;
-	mongoose.connect('mongodb://localhost:27017/StudentDB', {
-		useNewUrlParser: true
-	});
-	async function addStudent() {
-		//await newStudent.save();
-		await Student.findOneAndUpdate({'netid': netid}, {$set: {schedule: schedule}}, {upsert: true, useFindAndModify: false});
-		let students = await Student.find();
-		console.log(students);
-		res.status(200);
-		res.json({message: "worked and stuff"});
+server.post("/schedule/:netid", cors(corsOptions), (req, res) => {
+	try {
+		schedule = JSON.stringify(req.body);
+		netid = req.params.netid;
+		mongoose.connect('mongodb://localhost:27017/StudentDB', {
+			useNewUrlParser: true
+		});
+		async function addStudent() {
+			//await newStudent.save();
+			await Student.findOneAndUpdate({'netid': netid}, {$set: {schedule: schedule}}, {upsert: true, useFindAndModify: false});
+			let students = await Student.find();
+			console.log(students);
+			res.status(200);
+			res.json({message: "worked and stuff"});
+		}
+		console.log("about to add student");
+		addStudent();
+	} catch (e) {
+		res.status(501);
+		res.json({message: "failed"});
 	}
-	console.log("about to add student");
-	addStudent();
 })
-
-// GET
-
-// server.get("/majors", cors(corsOptions), (req, res) => {
-// 	res.json({"majors": data.majors});
-// });
-
-// server.get("/majorProgram/:majorID", cors(corsOptions), (req, res) => {
-// 	const majorID = req.params.majorID;
-
-// 	if (majorID == CS_ID) {
-// 		res.json({"program": data.csProgram});
-// 	} else if (majorID == ECON_ID) {
-// 		res.json({"program": data.econProgram});
-// 	} else {
-// 		res.status(404);
-// 		res.json({message: `Major ${majorID} doesn't exitst`});
-// 	}
-// });
-
-// server.get("/majorSchedule/:majorID", cors(corsOptions), (req, res) => {
-// 	const majorID = req.params.majorID;
-
-// 	if (majorID == CS_ID) {
-// 		res.json({"schedule": data.csSchedule});
-// 	} else if (majorID == ECON_ID) {
-// 		res.json({"schedule": data.econSchedule});
-// 	} else {
-// 		res.status(404);
-// 		res.json({message: `Major ${majorID} doesn't exitst`});
-// 	}
-// });
-
-// server.get("/schedule/:netid", cors(corsOptions), (req, res) => {
-// 	console.log("entered get");
-// 	mongoose.connect('mongodb://localhost:27017/StudentDB', {
-// 		useNewUrlParser: true
-// 	});
-// 	netid = req.params.netid;
-// 	Student.findOne({'netid': netid}, 'schedule', function(err, schedule) {
-// 		console.log("found netID1");
-// 		if (err) {
-// 			res.status(501);
-// 			res.json({message: "failed"});
-// 		} else {
-// 			res.status(200);
-// 			res.json(schedule.schedule);
-// 		}
-// 	})
-// })
-
-// // POST
-
-// server.post("/schedule/:netid", cors(corsOptions), (req, res) => {
-// 	schedule = JSON.stringify(req.body);
-// 	netid = req.params.netid;
-// 	mongoose.connect('mongodb://localhost:27017/StudentDB', {
-// 		useNewUrlParser: true
-// 	});
-// 	async function addStudent() {
-// 		//await newStudent.save();
-// 		await Student.findOneAndUpdate({'netid': netid}, {$set: {schedule: schedule}}, {upsert: true, useFindAndModify: false});
-// 		let students = await Student.find();
-// 		console.log(students);
-// 		res.status(200);
-// 		res.json({message: "worked and stuff"});
-// 	}
-// 	console.log("about to add student");
-// 	addStudent();
-// })
 
 // Start
 
